1dca33a4875a3a1b6f96e51a63a34451a3dfdc9b
Anuja Singh <singhanuja1227@gmail.com>
2025-05-29 20:45:18 +0530

    detailed card added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './Navbar';
 import PostCard from './PostCard';
 import Footer from './Footer';
 import Pagination from './Pagination';
+import PostDetail from './PostDetail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const [posts, setPost] = useState([]);
@@ -17,7 +20,7 @@ function App() {
       .then((d) => { setPost(d.data.posts); console.log(d); })
       .catch((error) => console.log('Error fetching data:', error));
   }, []);
-
+// using useEffect to fetch data as side effect wo
 
   const lastPostIndex = currPage * postPerPage;
   const firstPostIndex = lastPostIndex - postPerPage;
@@ -25,17 +28,32 @@ function App() {
   const currPosts = posts.slice(firstPostIndex,lastPostIndex);
 
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar />
-      <div className="posts-container">
+    <Routes>
+      <Route path = "/" element = {
+        <>
+         <div className="posts-container">
         {currPosts.map(post => (  // instead of displaying all data , only currPosts
           <PostCard key={post.id} post={post} />
         ))}
       </div>
-      <Pagination totalPosts = {posts.length} postsPerPage = {postPerPage} setCurrPage = {setCurrPage}
-      currentPage = {currPage}/>
+      <Pagination
+       totalPosts = {posts.length}
+        postsPerPage = {postPerPage} 
+        setCurrPage = {setCurrPage}
+      currentPage = {currPage}
+      />
+      </>
+      } />
+
+      <Route path = "/post/:id" element = {<PostDetail posts = {posts} />} />
+    </Routes>    
       <Footer />
     </div>
+    </Router>
+    
   );
 }
 
--- src/PostCard.js
+++ src/PostCard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './PostCard.css';
+import { Link } from 'react-router-dom';
 
 function PostCard(props) {
-  const { title, body, userId, reactions, tags } = props.post;
+  const {id, title, body, userId, reactions, tags } = props.post;
 
   return (
+    <Link to = {`/post/${id}`} className='post-link'>
     <div className="post-card">
-      <header className='card-header'>
+    
+        <header className='card-header'>
         <h2>{title}</h2>
         <p>{body}</p>
       </header>
       
-     
+      
+
      <div>
       <div className="sub-data">
         <span>User: {userId}</span>
@@ -19,7 +23,6 @@ function PostCard(props) {
           Likes: {reactions?.likes}, Dislikes: {reactions?.dislikes}
         </span>
       </div>
-
       <div className="tags">
         {tags?.map((tag, idx) => (
           <span key={idx} className="tag">#{tag}</span>
@@ -27,7 +30,8 @@ function PostCard(props) {
       </div>
      </div>
       
-    </div>
+    </div></Link>
+    
   );
 }
 
