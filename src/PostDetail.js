51244d7e139bbd866c3125b9896b16b1318e106a
Anuja Singh <singhanuja1227@gmail.com>
2025-06-09 15:02:23 +0530

    detailed_page_modified

--- src/PostDetail.js
+++ src/PostDetail.js
@@ -3,23 +3,38 @@ import './PostDetail.css';
 function PostDetail({ posts }) {
   const { id } = useParams();
   const post = posts.find(p => p.id === parseInt(id));
+ 
 
   if (!post) return <div>Post not found</div>;
 
   return (
-    <div className="post-detail">
-      <div>
-        <h2>{post.title}</h2>
+  <div className="detail-page-container">
+    <div className="center-content">
+      <div className="post-detail">
+        <header className='header'>
+          <h2>{post.title}</h2>
+        </header>
+
         <p>{post.body}</p>
+
+        <div className="tagss">
+          {post.tags.map((tag, idx) => (
+            <span key={idx} className="tagp">#{tag}</span>
+          ))}
+        </div>
+
+        <div className="subdata">
+          <span>User: 👤{post.userId}</span>
+          <div className='reaction'>
+            <span className='like-box'>👍{post.reactions.likes}</span>
+            <span className='dislike-box'>👎{post.reactions.dislikes}</span>
+          </div>
+        </div>
       </div>
-      
-      <div>
-        <p className='tags'><strong>Tags:</strong> {post.tags.join(', ')}</p>
-      <p><strong>Reactions:</strong> {post.reactions.likes} Likes, {post.reactions.dislikes} Dislikes</p>
-      </div>
-      
     </div>
-  );
+  </div>
+);
+
 }
 
 export default PostDetail;
